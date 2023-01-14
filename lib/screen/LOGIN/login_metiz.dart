e730a74f0cc3ac8c8ed01cefe45832ac3d053b75
nam54828 <nam54828@donga.edu.vn>
2023-01-14 15:13:04 +0700

    Chỉnh sửa form Register

--- lib/screen/LOGIN/login_metiz.dart
+++ lib/screen/LOGIN/login_metiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:metiz_cinema/components/app_bar_home.dart';
 import 'package:metiz_cinema/screen/LOGIN/login.dart';
+import 'package:metiz_cinema/screen/LOGIN/register.dart';
 
 class Login_Metiz extends StatefulWidget {
   const Login_Metiz({Key? key}) : super(key: key);
@@ -10,6 +11,23 @@ class Login_Metiz extends StatefulWidget {
 }
 
 class _Login_MetizState extends State<Login_Metiz> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  void showFaileMessage(){
+    showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text("Login"),
+          content: Text("Login failed"),
+          actions: [
+            TextButton(onPressed: (){
+              Navigator.pop(context, "OK");
+            }, child: Text("OK"))
+          ],
+        )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +44,165 @@ class _Login_MetizState extends State<Login_Metiz> {
       ),
       body: SingleChildScrollView(
         child: Container(
-
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 25),
+                  child: Image.asset('images/logo_metiz.png',
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 30, right: 30, top: 30),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Email *",
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10
+                          )
+                        ),
+                        style: TextStyle(
+                            color: Colors.white
+                        ),
+                        controller: emailController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return ("Trường hợp này không được để trống.");
+                          }
+                          if (!RegExp(
+                              "^[a-zA-Z0-9+_.-]+@[a-z0-9A-Z.-]+.[a-z]")
+                              .hasMatch(value)) {
+                            return ("Vui lòng nhập Email hợp lệ");
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          emailController.text = value!;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Mật khẩu *",
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10
+                          )
+                        ),
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                        controller: passwordController,
+                        validator: (value) {
+                          RegExp regex = RegExp(r'^.{6,}$');
+                          if (value!.isEmpty) {
+                            return ("Mật khẩu bắt buộc để đăng nhập");
+                          }
+                          if (!regex.hasMatch(value)) {
+                            return ("Mật khẩu không hợp lệ");
+                          }
+                        },
+                        onSaved: (value) {
+                          passwordController.text = value!;
+                        },
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style:
+                      ElevatedButton.styleFrom(primary: Colors.orangeAccent),
+                      child: SizedBox(
+                        width: 300,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            'ĐĂNG NHẬP',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50,),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Bạn chưa có tài khoản?",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 13
+                      ),),
+                    ],
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        child: Text("Đăng ký TÀI KHOẢN", style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold
+                        ),),
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Register()));
+                        },
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        child: Text("QUÊN MẬT KHẨU", style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontSize: 13,
+                          fontWeight: FontWeight.bold
+                        ),),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screen/LOGIN/register.dart
+++ lib/screen/LOGIN/register.dart
@@ -26,7 +26,210 @@ class _RegisterState extends State<Register> {
       ),
       body: SingleChildScrollView(
         child: Container(
-
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 30, right: 30, top: 30),
+                  child: Form(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Họ & Tên *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Ngày Sinh *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Email *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Số CMND *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Số Điện Thoại *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Nhập mật khẩu *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Nhập lại mật khẩu *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Chọn Thành Phố *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Chọn Quận/Huyện *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Địa Chỉ *",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.white
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 70),
+                          width: 30,
+                          child: Icon(Icons.square, color: Colors.orange,),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text("Tôi đã đọc hiểu, và đồng ý với các",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                            ),)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            InkWell(
+                              child: Text("Điều kiện & điều khoản",
+                              style: TextStyle(
+                                color: Colors.deepOrange,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12
+                              ),
+                              textAlign: TextAlign.left,),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 30,),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      MaterialButton(
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AppBarHome())),
+                        color: Colors.orange,
+                        minWidth: 330,
+                        height: 50,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: Text(
+                          "ĐĂNG KÝ",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
         ),
       ),
     );
